63654ef4c9c0e754d1f4c769f7c58b407aa85e50
scorpiopapa <liliang2005@gmail.com>
2014-08-01 16:24:09 +0800

    fix

--- spring-admin/src/main/java/com/joinway/admin/controller/DataGridController.java
+++ spring-admin/src/main/java/com/joinway/admin/controller/DataGridController.java
@@ -1,9 +1,14 @@
 package com.joinway.admin.controller;
 
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jsondoc.core.annotation.Api;
@@ -12,7 +17,9 @@ import org.jsondoc.core.annotation.ApiResponseObject;
 import org.jsondoc.core.pojo.ApiVerb;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -23,7 +30,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.joinway.appx.bean.view.DataGridView;
 import com.joinway.appx.bean.view.TableResultView;
-import com.joinway.appx.service.DataGridService;
+import com.joinway.appx.service.CustomTableService;
+import com.joinway.appx.service.TableService;
 import com.joinway.bean.logging.annotation.InputLog;
 import com.joinway.bean.logging.annotation.LogIgnore;
 import com.joinway.bean.logging.annotation.OutputLog;
@@ -40,7 +48,23 @@ public class DataGridController extends ExceptionController {
 	
 	private final static Logger log = LoggerFactory.getLogger(DataGridController.class);
 	
-	@Autowired DataGridService service;
+	@Autowired @Qualifier("DefaultTableService") TableService service;
+	
+	@Autowired(required=false) List<CustomTableService> customTableServices;
+	
+	Map<String, TableService> customServiceMap = new HashMap<String, TableService>();
+	
+	@PostConstruct
+	public void init(){
+		if(CollectionUtils.isNotEmpty(customTableServices)){
+			for(CustomTableService cts : customTableServices){
+				customServiceMap.put(cts.getTargetTableName().toLowerCase(), cts);
+				log.debug("added custom service {}", AopUtils.getTargetClass(cts).getCanonicalName());
+			}
+		}else{
+			log.info("no custom table service was found");
+		}
+	}
 	
 	@ApiMethod(path="app/search/{table}", verb=ApiVerb.GET, description=".........", produces=MediaType.APPLICATION_JSON_VALUE)
 	@ApiResponseObject
@@ -51,7 +75,7 @@ public class DataGridController extends ExceptionController {
 	@OutputLog
 	public DataGridView searchTable(@PathVariable("table") String table, @LogIgnore HttpServletRequest request) throws Exception {
 //		printParams(request);
-		return service.findRecord(table.toUpperCase(), request.getParameterMap());
+		return getHandler(table).findRecord(table.toUpperCase(), request.getParameterMap());
 	}
 
 	@ApiMethod(path="app/save/{table}", verb=ApiVerb.POST, description="....", produces=MediaType.APPLICATION_JSON_VALUE)
@@ -63,7 +87,7 @@ public class DataGridController extends ExceptionController {
 	@OutputLog
 	public TableResultView saveTable(@PathVariable("table") String table, @LogIgnore HttpServletRequest request) throws Exception {
 //		printParams(request);
-		return service.saveTable(table.toUpperCase(), request.getParameterMap());
+		return getHandler(table).saveTable(table.toUpperCase(), request.getParameterMap());
 	}
 	
 	@ApiMethod(path="app/delete/{table}", verb=ApiVerb.POST, description="....", produces=MediaType.APPLICATION_JSON_VALUE)
@@ -79,7 +103,7 @@ public class DataGridController extends ExceptionController {
 		
 		String[] arr = StringUtils.split(StringUtils.trimToEmpty(ids).replaceAll("\\s+", ""), ",");
 		if(ArrayUtils.isNotEmpty(arr)){
-			view = service.deleteTable(table.toUpperCase(), arr);
+			view = getHandler(table).deleteTable(table.toUpperCase(), arr);
 		}else{
 			view = new TableResultView();
 		}
@@ -87,6 +111,11 @@ public class DataGridController extends ExceptionController {
 		return view;
 	}
 	
+	TableService getHandler(String table){
+		TableService ts = customServiceMap.get(StringUtils.trimToEmpty(table).toLowerCase());
+		
+		return ts != null ? ts : service;
+	}
 	@Deprecated
 	void printParams(HttpServletRequest request){
 		Enumeration<String> enums = request.getParameterNames();
