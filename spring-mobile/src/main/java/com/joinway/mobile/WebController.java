8c1c9d8cebe8e1df37d75a3ed14a70b98a12f9c3
scorpiopapa <liliang2005@gmail.com>
2014-08-02 17:01:47 +0800

    fix multiple notice mass tab issue

--- spring-mobile/src/main/java/com/joinway/mobile/WebController.java
+++ spring-mobile/src/main/java/com/joinway/mobile/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("page")
 @Validated
 public class WebController {
 
